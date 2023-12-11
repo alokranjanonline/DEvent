72846bc7d7ccedbdb08b37cb5535d3aa65140b66
alokranjanonline <alokranjan999@gmail.com>
2023-12-11 22:40:38 +0530

    2

--- app/src/main/java/com/example/devent/StockDetails.kt
+++ app/src/main/java/com/example/devent/StockDetails.kt
@@ -1,14 +1,20 @@
 package com.example.devent
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class StockDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stock_details)
+        mAdView = findViewById(R.id.adView)
         val actionbar=supportActionBar
         val str = intent.getStringExtra("stockId")
         actionbar!!.title=intent.getStringExtra("stockName")
+        val stocksDetails = findViewById<TextView>(R.id.scriptDetails)
+        stocksDetails.text= intent.getStringExtra("stockDetails")
+        show_banner_ads(mAdView,this)
     }
 }
