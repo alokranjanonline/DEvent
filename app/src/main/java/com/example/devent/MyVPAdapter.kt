e30a3eb16a4fc40d497951b0e878b6c9c493a0fe
alokranjanonline <alokranjan999@gmail.com>
2023-12-12 22:36:37 +0530

    Create MyVPAdapter.kt

---
+++ app/src/main/java/com/example/devent/MyVPAdapter.kt
@@ -0,0 +1,21 @@
+package com.example.devent
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class MyVPAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa){
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position){
+            0->DividendFragment()
+            1->SplitFragment()
+            2->NewsFragment()
+            else->DividendFragment()
+        }
+    }
+
+}
